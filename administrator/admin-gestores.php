7310e5e2507c5dc9b5fd1bbb5197c1dd4cb07d96
Júlio Medeiros <juliohfmedeiros@gmail.com>
2018-05-29 21:28:41 +0000

    backend html

---
+++ administrator/admin-gestores.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Administração</title>
+    <link rel="stylesheet" href="../css/style_back.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div id="top-bar">
+      <a href="admin-imoveis.php"><img src="../img/logo.png" alt=""></a>
+      <div class="session-box-backend">
+        <div class="session-on">
+          <p>Bem-vindo, </p>
+          <p>Júlio Medeiros</p>
+          <button type="button" name="backend_logout">logout</button>
+        </div>
+        <div class="photo-employee">
+
+        </div>
+      </div>
+    </div>
+    <div class="nav_backend">
+      <a href="admin-imoveis.php">Imóveis</a>
+      <a href="admin-gestores.php">Gestores</a>
+      <a href="admin-estatisticas.php">Estatisticas</a>
+    </div>
+  </body>
+  <script type="text/javascript" src="../js/session.js"></script>
+</html>
---
+++ administrator/manager-estatisticas.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Gestão</title>
+    <link rel="stylesheet" href="../css/style_back.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div id="top-bar">
+      <a href="manager-imoveis.php"><img src="../img/logo.png" alt=""></a>
+      <div class="session-box-backend">
+        <div class="session-on">
+          <p>Bem-vindo, </p>
+          <p>Júlio Medeiros</p>
+          <button type="button" name="logout">logout</button>
+        </div>
+        <div class="photo-employee">
+
+        </div>
+      </div>
+    </div>
+    <div class="nav_backend">
+      <a href="manager-imoveis.php">Imóveis</a>
+      <a href="manager-estatisticas.php">Estatisticas</a>
+    </div>
+    <div class="">
+
+    </div>
+  </body>
+  <script type="text/javascript" src="../js/session.js"></script>
+</html>
