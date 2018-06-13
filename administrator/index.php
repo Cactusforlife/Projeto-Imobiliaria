cf84bef8c5a03678de9736b9390d6ab494513214
Cactusforlife <miguelsardinhapimentel@hotmail.com>
2018-06-13 20:25:30 +0000

    teste

--- administrator/index.php
+++
@@ -1,9 +0,0 @@
-<html lang="pt" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Gestão</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/style_back.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  </head>
