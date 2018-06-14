4e55e239098e56e3b883364bd966080c076858d8
Cactusforlife <miguelsardinhapimentel@hotmail.com>
2018-06-14 20:48:36 +0000

    teste

--- administrator/index.php
+++ administrator/index.php
@@ -21,7 +21,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <head>
     <meta charset="utf-8">
     <title>Backend - login</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="../css/style-back.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -31,33 +30,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="backend-container">
       <div class="login-backend">
         <form class="" action="index.html" method="post">
-          <label for="">email</label> <input type="email" name="">
-          <label for="">password</label> <input type="password" name="">
-          <input type="button" name="" value="login">
+          <label for="">email</label> <input type="email" name="email">
+          <label for="">password</label> <input type="password" name="password">
+          <input type="submit" name="" value="login">
         </form>
       </div>
     </div>
   </body>
-=======
-
-</head>
-  <body>
-
-    <form action="" method ="POST">
-
-    <label for="Email"> Email:</label>
-
-      <input name="email" type="text">
-
-      <label for="Email"> Password:</label>
-
-      <input name="password" type="password">
-
-    <input type="submit" value="login" required>
-
-    </form>
-
-</body>
-
->>>>>>> dev-miguel
 </html>
